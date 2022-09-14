2be9ce57cce88a168c1dc8c85371201a943d5a7b
José Orlando de Carvalho Júnior <juniororlandocv@gmail.com>
2022-09-14 10:46:36 -0300

    C:/Program Files/Git/projeto/sprint 8/m1-entrega-capstone-ecommerce-JUorlando

--- js/database.js
+++ js/database.js
@@ -3,7 +3,7 @@
 const data = [
   {
     id: 1,
-    img: "./img/img.png",
+    img: "./img/Nike.png",
     nameItem: "Air Jordan 1",
     description:
       "O Air Jordan 1 tem alta relevância no meio sneakerhead...",
@@ -13,7 +13,7 @@ const data = [
   },
   {
     id: 2,
-    img: "/img/Jordan 4.jpg",
+    img: "./img/Jordan 4.jpg",
     nameItem: "Air Jordan 4",
     description:
       "Um dos modelos mais cobiçados ganha um toque vintage...",
@@ -53,7 +53,7 @@ const data = [
   },
   {
     id: 6,
-    img: "./img/Converse.png",
+    img: "./img/converse.png",
     nameItem: "Converse",
     description:
       "Estiloso na medida certa para fazer a diferença no seu dia a dia!...",
